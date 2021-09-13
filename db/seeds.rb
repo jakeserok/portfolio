ad388d88a38d78226904ef02b870a6c64b2d5014
jakeserok <jakeserok@gmail.com>
2021-09-13 19:51:52 -0400

    Updated seed file, rearranged pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,21 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Project.create(
-    name: "To Do List", 
-    app: "https://serok-todo.herokuapp.com",
-    repo: "https://github.com/jakeserok/to_do",
-    description: "Simple to-do list application created which allows users to create
-    a goal and sub tasks for the goal. These tasks can be marked as completed, which
-    will then cross it off the list utilizing jquery. The background of the app can be
-    changed with a toggle that is located in the navbar, allowing users to switch from 
-    light mode to dark mode. This feature uses session data to know which background to
-    display."
-  )
+  name: "To Do List", 
+  app: "https://serok-todo.herokuapp.com",
+  repo: "https://github.com/jakeserok/to_do",
+  description: "Simple to-do list application created which allows users to create
+  a goal and sub tasks for the goal. These tasks can be marked as completed, which
+  will then cross it off the list utilizing jquery. The background of the app can be
+  changed with a toggle that is located in the navbar, allowing users to switch from 
+  light mode to dark mode. This feature uses session data to know which background to
+  display."
+)
+Project.create(
+  name: "Twitter Manager",
+  app: "https://twitter--manager.herokuapp.com",
+  repo: "https://github.com/jakeserok/social_media_manager",
+  description: "This application allows users to create an account or sign in via google. 
+  Once signed in, they can link their Twitter account and schedule tweets to be sent out
+  at a specific time, which is handled in the background via Redis."
+)
